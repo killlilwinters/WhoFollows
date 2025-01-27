e34a052781eda9ab0e32cf37d8ce8c8719a16a03
MaksGorobets <gorobets542@gmail.com>
2025-01-27 23:31:14 +0200

    Initial Commit

---
+++ WhoFollows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoFollows
+//
+//  Created by Maks Winters on 27.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
