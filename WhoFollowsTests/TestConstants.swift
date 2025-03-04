71dadd2a2af589976fcbde2f2361aa0cc471d045
MaksGorobets <gorobets542@gmail.com>
2025-03-05 00:25:49 +0200

    import WhoFollows module for TestConstants

--- WhoFollowsTests/TestConstants.swift
+++ WhoFollowsTests/TestConstants.swift
@@ -5,4 +5,6 @@
 //  Created by Maks Winters on 04.03.2025.
 //
 
+@testable import WhoFollows
+
 let follower = Follower(login: "killlilwinters", avatarUrl: "https://avatars.githubusercontent.com/u/139014119?v=4")
